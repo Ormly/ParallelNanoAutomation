921f22a60ee4d2d65b41a261706895c6ed6abdd4
Tim <jeditim@hotmail.co.uk>
2020-10-23 17:42:22 +0200

    Update system_controller_node_setup.bash
    Corrected fstab file

--- system_controller_node_setup.bash
+++ system_controller_node_setup.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
 #Updates, timezone and hostname
-echo "Waiting for network..."
-until ping -c1 www.google.com >/dev/null 2>&1; do :; done
-apt update -y
 apt full-upgrade -y
+apt-get install nfs-common gcc g++ git make mpich openssh-server build-essential python3-pip libffi-dev -y
 timedatectl set-timezone Europe/Berlin
-hostnamectl set-hostname lisa
 
 #NIS setup
 echo "nis nis/domain string pjama" > /tmp/nisinfo
@@ -16,8 +13,8 @@ rm /tmp/nisinfo
 cat > /etc/nsswitch.conf << EOF
 #
 # Example configuration of GNU Name Service Switch functionality.
-# If you have the \`glibc-doc-reference' and \`info' packages installed, try:
-# \`info libc "Name Service Switch"' for information about this file.
+# If you have the \\\`glibc-doc-reference' and \\\`info' packages installed, try:
+# \\\`info libc "Name Service Switch"' for information about this file.
 
 passwd:         compat nis
 group:          compat nis
@@ -39,6 +36,8 @@ cat > /etc/rc.local << EOF
 # start nis related services
 systemctl restart rpcbind
 systemctl restart nis
+python3 /nfs/scripts/ParallelNano_Lisa_Beacon/beacon_server/beacon_server_daemon.py
+gunicorn -w 2 /nfs/scripts/ParallelNano_Lisa_Lighthouse/wsgi:app --daemon
 exit 0
 EOF
 
@@ -88,21 +87,18 @@ cat > /etc/fstab << EOF
 #
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
 # / was on /dev/sda1 during installation
-UUID=4518aadf-afc5-4d05-bb53-cf351d9ad522 /               ext4    errors=remount-ro 0       1
+UUID=$(blkid -s UUID -o value /dev/sda1) /               ext4    errors=remount-ro 0       1
 /swapfile                                 none            swap    sw              0       0
 # pjama related mounts
 bobby:/nfs/home /nfs/home nfs rw,soft,x-systemd.automount 0 0
 bobby:/nfs/scripts /nfs/scripts nfs rw,soft,x-systemd.automount 0 0
 EOF
+
 mkdir /nfs /nfs/home /nfs/scripts
 mount bobby:/nfs/scripts /nfs/scripts
-
-#Finishing up
-apt-get install openssh-server build-essential git python3-pip libffi-dev -y
 
 cd /nfs/scripts/ParallelNano_Lisa_Beacon
 python3 setup.py install --user
 
 cd /nfs/scripts/ParallelNano_Lisa_Lighthouse
 python3 setup.py install --user
-cd ~
