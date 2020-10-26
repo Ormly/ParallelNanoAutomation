b2f1e23c6a40806ee4851491d6221713cc281be4
Tim <jeditim@hotmail.co.uk>
2020-10-26 23:54:54 +0100

    Chmod recursion

--- compute_node_setup.bash
+++ compute_node_setup.bash
@@ -118,7 +118,7 @@ cd /opt/
 wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.5.tar.gz -O openmpi.tar.gz
 tar -xzvf openmpi.tar.gz
 rm openmpi.tar.gz
-chmod +x openmpi-4.0.5/
+chmod +x -R openmpi-4.0.5/
 cd openmpi-4.0.5/
 ./configure --prefix=/usr/local --enable-heterogeneous
 make all install
