b478fcbdb33ed444c0233c47bba69e08e8d49468
Tim <jeditim@hotmail.co.uk>
2020-11-02 11:32:20 +0100

    Update system_controller_node_setup.bash
    Added pjamaadmin to sudoers

--- system_controller_node_setup.bash
+++ system_controller_node_setup.bash
@@ -109,6 +109,40 @@ bobby:/nfs/home /nfs/home nfs rw,soft,x-systemd.automount 0 0
 bobby:/nfs/scripts /nfs/scripts nfs rw,soft,x-systemd.automount 0 0
 EOF
 
+cat > /etc/sudoers << EOF
+#
+# This file MUST be edited with the 'visudo' command as root.
+#
+# Please consider adding local content in /etc/sudoers.d/ instead of
+# directly modifying this file.
+#
+# See the man page for details on how to write a sudoers file.
+#
+Defaults	env_reset
+Defaults	mail_badpass
+Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
+
+# Host alias specification
+
+# User alias specification
+
+# Cmnd alias specification
+
+# User privilege specification
+root	ALL=(ALL:ALL) ALL
+pjamaadmin ALL=(ALL) ALL
+
+# Members of the admin group may gain root privileges
+%admin ALL=(ALL) ALL
+
+# Allow members of group sudo to execute any command
+%sudo	ALL=(ALL:ALL) ALL
+
+# See sudoers(5) for more information on "#include" directives:
+
+#includedir /etc/sudoers.d
+EOF
+
 mkdir /nfs /nfs/home /nfs/scripts
 mount bobby:/nfs/scripts /nfs/scripts
 
