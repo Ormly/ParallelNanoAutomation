b6b244a46d6d4366e25f6d48e0f93a48753f2be1
Tim <jeditim@hotmail.co.uk>
2020-11-04 09:56:49 +0100

    Create git_setup.bash
    Added git setup to clone

---
+++ git_setup.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~
+git clone git@github.com:Ormly/ParallelNanoAutomation.git automation
+git clone git@github.com:Ormly/ParallelNano_Lisa_Beacon.git beacon
+git clone git@github.com:Ormly/ParallelNano_Lisa_Beacon_Agent.git beacon_agent
+git clone git@github.com:Ormly/ParallelNano_Lisa_Lighthouse.git lighthouse
+git clone git@github.com:Ormly/ParallelNano_Lisa_Tempo.git tempo
+git clone git@github.com:Ormly/ParallelNanoShowcase.git showcase
+chmod 775 -R *
