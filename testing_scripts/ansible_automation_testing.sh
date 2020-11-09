d4f8cbf1eb3bce4907df04445a2c1e39e2737761
Eva <katalenca@gmail.com>
2020-11-09 17:21:14 +0100

    finished ansible testing cases

--- testing_scripts/ansible_automation_testing.sh
+++ testing_scripts/ansible_automation_testing.sh
@@ -7,9 +7,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+loop_num=(1 2 3)
+
 #Start tests-----------------------------------
 echo "Starting Ansible automation testing"
 
+#1
 #Check if ansile is installed
 ansible_status=$(dpkg -s ansible)>/dev/null
 if [[ $ansible_status == *"Status: install ok installed"* ]]; then
@@ -19,6 +22,8 @@ else
 	exit 2
 fi
 
+
+#2
 #Install_apt_package.yml
 #Check the package is not install
 echo "Testing Install_apt_package.yml playbook..."
@@ -48,6 +53,7 @@ else
 fi
 
 
+#3
 #Remove_apt_package.yml
 echo "Testing Remove_apt_package.yml playbook..."
 #Check the package is installed
@@ -71,6 +77,8 @@ else
 	fi
 fi
 
+
+#4
 #Kickstart_compute_node.yml
 echo "Testing Kickstart_computer_node.yml playbook..."
 #Run playbook in a “pure johnny”
@@ -84,8 +92,10 @@ else
 	exit 5
 fi
 
+
+#5
 #Kickstart_control_node.yml
-#echo "Testing Kickstart_control_node.yml playbook..."
+echo "Testing Kickstart_control_node.yml playbook..."
 #Run playbook in a “pure lisa”
 kickstart_status=$(ansible-playbook /nfs/scripts/automation/playbooks/kickstart_control_node.yml -i "/nfs/scripts/automation/inventory.ini" -e target=controller)
 #Run testing lisa script
@@ -97,33 +107,89 @@ else
 	exit 5
 fi
 
+
+#6
 #Reboot.yml
 echo "Testing Reboot.yml playbook..."
-#Record current time (T1)
-#Run playbook
-#Check up time and current time (T2), to see whether the uptime = T2 - T1
-#(T1 and T2 maybe not that accurate, maybe just check the up time at the end??)
+#record current uptime of nodes
+uptime_vars=()
 
+for n in ${loop_num[@]}
+do
+	echo "Loop num $n"
+	temp1=$(ssh johnny$n "uptime")
+	temp1=${temp1##*"up"}
+	uptime_vars+=${temp1%%,*}
+done
+
+#Run playbook
+#ansible-playbook /nfs/scripts/automation/playbooks/reboot.yml -i "/nfs/scripts/automation/inventory.ini" -e target=nodes
+#Check up-time
+for m in ${loop_num[@]}
+do
+	temp2=$(ssh johnny$m "uptime")
+	temp2=${temp2##*"up"}
+	temp2=${temp2%%,*}
+	temp2=$(echo $temp2 | tr -d :)
+
+	#convert values into nums
+	uptime_vars[$m-1]=$(echo ${uptime_vars[$m-1]} | tr -d :)
+	
+	if [[ $temp2 -gt ${uptime_vars[$m-1]} ]]; then
+		echo -e "$RED ERROR: Reboot not succesful $NC"
+	else
+		echo -e "$GREEN Reboot sucessful $NC"
+	fi
+done
+
+
+#7
 #Shutdown.yml
 echo "Testing Shutdown.yml playbook..."
 #Ping the machine to make sure it is online
-for var in 1 2 3 4 5 6 7 8
+johnny_status=()
+for var in $loop_num[@]
 do
 	johnnyX=$(host johnny$var) >/dev/null
 	if [[ $? -eq 0 ]]; then
 		ping -q -c 1 johnny$var ;
 		if [[ $? -eq 0 ]]; then
-			echo -e "$GREEN johnny$var reachable $NC"
+			echo -e "$johnny$var: $GREEN UP $NC"
+			johnny_status+=(1)
 		else
-			echo -e "$RED ERROR: johnny$var unreachable $NC" 1>&2
-			exit 12
+			echo -e "$johnny$var: $RED DOWN $NC"
+			johnny_status+=(0)
+		fi
+	fi
+done
+#Run playbook
+ansible-playbook /nfs/scripts/automation/playbooks/reboot.yml -i "/nfs/scripts/automation/inventory.ini" -e target=nodes
+#Ping the machine to see if it is shut down
+for t in $johnny_status[@]; 
+do
+	if [[ t -eq 1 ]]; then
+		ping -q -c 1 johnny[@] 
+		if [[ $? -neq 0 ]]; then
+			echo -e "$johnny[@]: $GREEN SHUTDOWN $NC"
+		else
+			echo -e "$RED ERROR: johnny[@] not properly shut down $NC"
+			exit 7
 		fi
 	fi
 done
 
-#Run playbook
-#Ping the machine to see if it is shut down
+#Turn on the nodes again
+for t in $loop_num;
+do
+		cd /nfs/scripts/automation/lisa_scripts
+		python3 power_control.py power $t
+		echo "Turning on johnny$t again"
+done
 
+echo "All johnnys turned back on, waiting for them to be responsive"
+sleep(1000)
+
+#8
 #Update_upgrade.yml
 echo "Testing Update_upgrade.yml playbook..."
 #Run playbook
