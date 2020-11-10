9059f980dba2f0bee1b88dcd1527a4ebbe2729dd
Tim <jeditim@hotmail.co.uk>
2020-11-10 12:23:27 +0100

    Removed GUI from Johnny

--- compute_node_setup.bash
+++ compute_node_setup.bash
@@ -4,6 +4,7 @@ until apt update -y > /dev/null 2>&1; do :; done
 until apt full-upgrade -y > /dev/null 2>&1; do :; done
 apt-get install nfs-common gcc g++ git make mpich openssh-server build-essential python3-pip libffi-dev -y
 timedatectl set-timezone Europe/Berlin
+systemctl set-default multi-user.target
 
 #NIS setup
 echo "nis nis/domain string pjama" > /tmp/nisinfo
