356b0f476ab507f6993f27fa38a7249c2f54343e
Tim <jeditim@hotmail.co.uk>
2020-11-12 16:55:06 +0100

    Added key back to root

--- server_node_setup.bash
+++ server_node_setup.bash
@@ -790,6 +790,10 @@ cat > known_hosts << EOF
 |1|y5U42fljybY+cedPxOndkAY5s0Q=|ZtdYfVuh7P9psIQJzaFGYS6fO4c= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
 EOF
 
+mkdir -p /root/.ssh
+mv id_rsa /root/.ssh/id_rsa
+mv known_hosts /root/.ssh/known_hosts
+
 mkdir /nfs/scripts/
 cd /nfs/scripts/
 git clone git@github.com:Ormly/ParallelNanoAutomation.git automation
