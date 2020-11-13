ee5f3cdec479e11ddcea888a5afbff29ca4c3d16
Tim <jeditim@hotmail.co.uk>
2020-11-13 18:26:50 +0100

    Undone checkhostup no because of reservations

--- create_user.bash
+++ create_user.bash
@@ -61,15 +61,6 @@ cat > config << EOF
 Host github.com
 	IdentityFile ~/.ssh/git_rsa
 	User git
-
-Host johnny\d\d
-  CheckHostIP no
-
-Host lisa
-  CheckHostIP no
-
-Host bobby
-  CheckHostIP no
 EOF
 ssh-keygen -f id_rsa -q -N ""
 cp id_rsa.pub authorized_keys
